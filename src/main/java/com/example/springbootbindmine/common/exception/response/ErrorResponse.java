84d4dd97e8139dd03342b4c256529a86a4199b63
chanhyub <chan991028@naver.com>
2025-01-07 18:46:23 +0900

    ErrorResponse annotation 수정

--- src/main/java/com/example/springbootbindmine/common/exception/response/ErrorResponse.java
+++ src/main/java/com/example/springbootbindmine/common/exception/response/ErrorResponse.java
@@ -2,19 +2,14 @@ package com.example.springbootbindmine.common.exception.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.validation.FieldError;
 
 import java.util.List;
 
 @Builder
-@Getter
-@RequiredArgsConstructor
 public record ErrorResponse(Integer status, String code, String message, @JsonInclude(JsonInclude.Include.NON_EMPTY) List<ValidationError> errors) {
-    @Getter
+
     @Builder
-    @RequiredArgsConstructor
     public record ValidationError(String field, String message) {
         public static ValidationError of(final FieldError fieldError) {
             return ValidationError.builder()
