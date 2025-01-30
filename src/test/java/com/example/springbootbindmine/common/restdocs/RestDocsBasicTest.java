79cc775d962021db0b276fccf9cc13eb615c7670
chanhyub <chan991028@naver.com>
2025-01-30 18:08:01 +0900

    openAPI3 기반 restdocs + swagger 에서 restdocs로 변경

---
+++ src/test/java/com/example/springbootbindmine/common/restdocs/RestDocsBasicTest.java
@@ -0,0 +1,42 @@
+package com.example.springbootbindmine.common.restdocs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@Import(RestDocsConfig.class)
+@ExtendWith(RestDocumentationExtension.class)
+@ActiveProfiles("test")
+public abstract class RestDocsBasicTest {
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Autowired
+    public RestDocumentationResultHandler restDocs;
+
+    @Autowired
+    public ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(final WebApplicationContext context, final RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
+                .alwaysDo(MockMvcResultHandlers.print())
+                .alwaysDo(restDocs)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+}
---
+++ src/test/java/com/example/springbootbindmine/common/restdocs/RestDocsConfig.java
@@ -0,0 +1,20 @@
+package com.example.springbootbindmine.common.restdocs;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@TestConfiguration
+public class RestDocsConfig {
+    @Bean
+    public RestDocumentationResultHandler restDocsMockMvcConfigurationCustomizer() {
+        return MockMvcRestDocumentation.document("{class-name}/{method-name}",
+                Preprocessors.preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()));
+    }
+}
